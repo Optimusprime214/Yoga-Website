582035601aa3fcd81644fb6c2c9d843944ab51a1
Debartha Sarkar <debarthasarkar214@gmail.com>
2024-10-29 13:14:15 +0530

    2nd Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+<<<<<<< HEAD
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        'input-left': '5px 0px 0px 5px',
+        'button-right': '0px 5px 5px 0px',
+      }
+    },
+=======
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+>>>>>>> 4a06074 (first commit)
+  },
+  plugins: [],
+}
+
